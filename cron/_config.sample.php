bb84c20f029fdc7ebbeff90c3d79478f5a9c6087
Steffen Manzer <git@noecho.de>
2019-11-11 13:59:42 +0100

    Aggregiere Auftragsbücher regelmäßig und prüfe auf Lücken >5s

--- cron/_config.sample.php
+++ cron/_config.sample.php
@@ -19,18 +19,27 @@ Datenbank-Tabellenstrukturen:
 -- order_books
 -- ENUM für exchange_name und source_name spart erheblich Speicherplatz
 CREATE TABLE `order_books` (
-  `book_time` datetime(6) NOT NULL COMMENT 'Zeitstempel der Abfrage, Zeitzone UTC (!)',
+  `book_time` datetime(2) NOT NULL COMMENT 'Zeitstempel der Abfrage, Zeitzone UTC (!)',
   `exchange_name` enum('bitfinex_usd','bitfinex_eur','bitstamp_usd','bitstamp_eur','coinbase_usd','coinbase_eur') NOT NULL,
   `source_host` enum('drive.noecho.de') NOT NULL,
   `type` enum('bid','ask') NOT NULL,
   `price` decimal(10,4) NOT NULL COMMENT 'Preis typischerweise in xxxx.yy, aber zur Sicherheit etwas Puffer: xxxxxx.yyyy',
   `amount` decimal(16,8) NOT NULL COMMENT 'Bitcoins bis zu 1 sat (1/8)'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPRESSED;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPRESSED COMMENT='Rohdaten der abgefragten Orderbuecher';
 
 ALTER TABLE `order_books`
   ADD KEY `book_time` (`book_time`),
-  ADD KEY `exchange_name` (`exchange_name`),
-  ADD KEY `source_host` (`source_host`);
+  ADD KEY `exchange_name` (`exchange_name`);
 
+
+CREATE TABLE `order_books_aggregate` (
+  `book_time` datetime NOT NULL COMMENT 'Zeitstempel der Abfrage, Zeitzone UTC (!)',
+  `exchange_name` enum('bitfinex_usd','bitfinex_eur','bitstamp_usd','bitstamp_eur','coinbase_usd','coinbase_eur') NOT NULL,
+  `bid` decimal(10,4) NOT NULL COMMENT 'Preis typischerweise in xxxx.yy, aber zur Sicherheit etwas Puffer: xxxxxx.yyyy',
+  `bid_amount` decimal(16,8) NOT NULL COMMENT 'Bitcoins bis zu 1 sat (1/8)',
+  `ask` decimal(10,4) NOT NULL COMMENT 'Preis typischerweise in xxxx.yy, aber zur Sicherheit etwas Puffer: xxxxxx.yyyy',
+  `ask_amount` decimal(16,8) NOT NULL COMMENT 'Bitcoins bis zu 1 sat (1/8)',
+  KEY `exchange_name` (`exchange_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 ROW_FORMAT=COMPRESSED COMMENT='Jeweils beste Bid- und Ask-Orders und deren Mengen';
 
 */
