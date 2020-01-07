41c564a78b8cf72ac06715a7c27b7aed58ea34b6
fox34 <github@noecho.de>
2020-01-07 15:49:08 +0100

    Alte Kommentare entfernt

--- cron/coinbase-tick.php
+++ cron/coinbase-tick.php
@@ -72,23 +72,6 @@ $url = API_URL . '?' . http_build_query([
 
 echo 'Querying ' . $url . PHP_EOL;
 
-<<<EOCOMMENT
-$c = curl_init($url);
-curl_setopt_array($c, [
-    \CURLOPT_HEADER => true,
-    \CURLOPT_RETURNTRANSFER => true,
-    \CURLOPT_HTTPHEADER => ["User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Safari/605.1.15",
-            "Accept-Language: de-de",
-            "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-            "Accept-Encoding: gzip, deflate, br",
-            "Connection: Close"]
-]);
-
-$json = curl_exec($c);
-var_dump($json);
-exit;
-EOCOMMENT;
-
 $json = file_get_contents($url, false, stream_context_create([
     'http' => [
         'header' => "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_2) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.4 Safari/605.1.15\r\n" . 
